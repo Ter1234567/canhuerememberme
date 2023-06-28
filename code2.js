a73176650082033388201f396902a4f3d91af32f
Ter1234567 <137788408+Ter1234567@users.noreply.github.com>
2023-06-28 20:45:49 +0800

    code2.js

--- code2.js
+++ code2.js
@@ -38,7 +38,7 @@ function checkAnswer(currentLevel) {
     setTimeout(function() {
       $("body").removeClass("game-over");
     }, 200);
-    $("#level-title").text("Game Over, Press Any Key to Restart");
+    $("#level-title").text("GAME OVER, Press Any Key to Restart");
     startOver();
   }
 }
